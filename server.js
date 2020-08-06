8d804878eefbe5332a4e429e8f7716b1d8228761
Abhisheikh-G <abhisheikhg@gmail.com>
2020-08-06 11:29:57 -0400

    Set up initial node api routes/controllers

--- server.js
+++ server.js
@@ -3,23 +3,37 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+//routes
+const blogRoutes = require("./routes/blog");
 
 //app
 const app = express();
 
-// middlewares
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Database connected..`));
 
+// middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
-//cors
-app.use(cors());
 
-//routes
-app.get("/api", (req, res) => {
-  res.json({ time: Date().toString() });
-});
+//cors
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+//routes middleware
+app.use("/api", blogRoutes);
 
 //port
 const port = process.env.Port || 8000;
